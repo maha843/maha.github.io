edf863dfdd791c086863ce204b24f0714b309457
maha843 <74098424+maha843@users.noreply.github.com>
2020-11-07 14:47:00 +0300

    hi
    hi

---
+++ wwwroot/js.js
@@ -0,0 +1,44 @@
+﻿
+function myFunction() {
+    document.getElementById("myForm").submit();
+}
+function myonclickfn() {
+    document.location.herf = "sp.html";
+}
+
+
+    function Validate() {
+            var password = document.getElementById("p").value;
+            var confirmPassword = document.getElementById("cp").value;
+            if (password != confirmPassword) {
+        alert("Passwords do not match.");
+    return false;
+}
+return true;
+    }
+
+
+function change() {
+    document.getElementById("img").src = "p2.jpg";
+}
+
+
+
+var item = document.getElementById("myLI");
+
+function removeLi() {
+    item.parentNode.removeChild(item);
+}
+
+function appendLi() {
+    var list = document.getElementById("myList");
+    list.appendChild(item);
+}
+function show_price(){
+    Var stadpe = document.stadprice.value;
+    Var stad1= document.getElementById("stad1");
+    stad1.innerhtml = "staduim price is " + stadpe ;
+
+
+
+}
